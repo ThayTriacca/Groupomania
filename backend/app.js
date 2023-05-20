85047924e0b7532835ee4cb6e21f7046a161ec86
ThayTriacca <thay_triacca@hotmail.com>
2023-05-20 17:29:56 +0100

    Adding env

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-const db = new Sequelize('gpmdb', 'root', '1234', {
-  host: 'localhost',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
